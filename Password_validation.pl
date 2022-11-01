c2802d6c851254933ac17922627218b830bb4316
Naruto123Jiraiya <115707956+Naruto123Jiraiya@users.noreply.github.com>
2022-11-01 12:13:38 +0530

    Add files via upload

---
+++ Password_validation.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use 5.010;
+#use strict;
+use warnings;
+
+#print "Hello World!\n";
+print"enter password:";
+chomp($pass=<>);
+if($pass=~ /^([a-zA-Z].*){8,14}$/){
+	print"$pass is valid\n";
+}
+else{
+print"$pass is not valid\n";	
+}
